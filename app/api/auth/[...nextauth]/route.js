495d1cd12ff15708172c00d1b3f8c946bfab7bd6
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-05-27 17:26:32 +0300

    create api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -40,7 +40,22 @@ const authOptions = {
     secret: process.env.NEXTAUTH_SECRET, 
     pages: {
         signIn: '/login',
-    }
+    },
+
+    callbacks: {
+        async jwt({ token, user }) {
+          if (user) {
+            token.id = user.id;
+          }
+          return token;
+        },
+        async session({ session, token }) {
+          if (token) {
+            session.user.id = token.id;
+          }
+          return session;
+        }
+}
 };
 
 // Create NextAuth handler
